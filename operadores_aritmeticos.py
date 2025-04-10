7a9d249fce5a2e0017fd5090a7289659cb3ff3ca
jaquelinechurqui <jaqueline.churqui@ucb.edu.bo>
2025-04-10 09:48:51 -0400

    Add files via upload

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,35 @@
+
+#CREAR UNA VAR
+saludo = "Hola Mundo!!!"
+print(saludo)
+
+
+#suma de numeros
+num1 = 10
+num2 = 20
+suma = num1 + num2
+print(suma)
+
+#suma de palabras
+palabra1 = "hola"
+palabra2 = "mundo"
+suma_palabras = palabra1 + " " + palabra2
+print(suma_palabras)
+
+#resta de numeros
+num1 = 4
+num2 = 50
+resta = num1 - num2
+print(resta)
+
+#multiplicacion de numeros
+num1 = 12
+num2 = 12
+multiplicacion = num1 * num2
+print(multiplicacion)
+
+#division de numeros
+num1 = 80
+num2 = 40
+division = (int(num1/num2))
+print(division)
