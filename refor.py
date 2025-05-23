668b8badd845a69c6229972614f61d1c48148294
jaquelinechurqui <jaqueline.churqui@ucb.edu.bo>
2025-05-23 19:13:34 -0400

    Add files via upload

--- refor.py
+++ refor.py
@@ -87,3 +87,103 @@ if pin == pin_correcto:
          print("opcion invalida")
 else:
     print("Pin incorrecto")
+
+
+#Analisis de Digitos de un Numero
+can_digit = 0
+suma = 0 
+mayor = 0
+pares = 0
+digito = input("Ingrese un numero entero positivo:")
+for digitos in digito:
+    digit = int(digitos)
+    can_digit +=1
+    suma += digit
+
+    if digit % 2 == 0:
+        pares +=1
+
+    if digit > mayor:
+        mayor = digit
+print (f"la cantidad de digitos  son: {can_digit}")
+print (f"La suma de sus digitos son: { suma}")
+print (f"los digitos pares son:{pares}")
+print(f"el digito mayor es: {mayor}")
+
+#Contador de Vocales y Consonantes
+vocales = 0
+consonantes = 0 
+palabra = input ("Ingrese una frase:")
+for frase in palabra :
+    frase = frase.lower()
+    if frase.isalpha():
+        if frase in "aeiou":
+            vocales +=1
+        else:
+            consonantes += 1
+print(f"La frase tiene: {vocales}vocales")
+print(f"La frase tiene: {consonantes}consonantes")
+
+#Ordenamiento de Notas
+notas = []
+for i in range (1,6):
+    nota = float(input(f"Ingrese la nota del estudiante{i}:"))
+    notas.append(nota)
+
+notas.sort()
+print("Notas ordenadas de menor a mayor:")
+for nota in notas:
+    print(nota)
+
+prom = sum(notas)/ len(notas)
+print(f"El promedio de sus calificaciones es: {prom}")
+
+
+#Numero  ́Magico por Mitades
+numero = input("Ingrese un numero de al menos dos cifras:")
+
+largo = len(numero)
+mitad = largo //2
+
+if largo % 2 == 0 :
+    izquierda = numero[:mitad]
+    derecha = numero[mitad:]
+else:
+    izquierda = numero [:mitad]
+    derecha = numero [mitad + 1:]
+
+suma_derecha = 0
+suma_izquierda = 0
+
+for deren in derecha:
+    suma_derecha += int(deren)
+
+for izqui in izquierda:
+    suma_izquierda += int(izqui)
+
+print(f"La suma del lado derecho es: {suma_derecha}")
+print(f"La suma del lado izquierdo es: {suma_izquierda}")
+if suma_derecha == suma_izquierda:
+    print("Si es un numero magico")
+else:
+    print("No es un numero magico")
+
+#Suma Alternada de Dıgitos
+numero = input("Ingrese un numero")
+
+suma = 0
+expresion = ""
+
+for i in range(len(numero)):
+    digito = int(i)
+    if i == 0:
+        suma +=digito
+        expresion += str(digito)
+    if i % 2 == 0:
+        suma -= digito
+        expresion += "-"+ str(digito)
+    else:
+        suma += digito
+        expresion += "+"+ str(digito)
+
+print(expresion + "=" + str(suma))
